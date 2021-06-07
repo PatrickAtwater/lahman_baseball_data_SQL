8caa81db574361925dea83d212c1e62d12d69d32
SamSitarek <sam.sitarek@gmail.com>
2021-06-07 15:50:12 -0500

    EOD1 q1 solved

--- lehman_baseball_project.sql
+++ lehman_baseball_project.sql
@@ -1,2 +1,32 @@
+/*
+Q1
+*/
+
+SELECT MIN(debut), MAX (finalGame)
+FROM people
+
 SELECT *
-FROM
+FROM people
+ORDER BY debut DESC
+
+--1871 to 2017
+
+SELECT*
+FROM people AS p 
+INNER JOIN batting AS b
+ON p.playerid=b.playerid
+WHERE b.playerid = 'torrejo02'
+
+SELECT 
+MAX (year),
+MIN(year),
+MAX (year)-MIN(year) AS span_of_years
+FROM homegames;
+
+--145 YEARS (also says 1st day of opening season 2017)
+
+select MIN(span_first), MAX(span_last),
+MAX(span_last) - MIN(span_first) as span_of_days
+from homegames
+
+--53112 days
